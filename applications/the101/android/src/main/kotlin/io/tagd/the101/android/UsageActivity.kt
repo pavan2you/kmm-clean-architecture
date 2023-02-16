676af9f2b4b33268f4ed67e70316c28ba7f0d067
pavan kunchapu <pavan.k@verse.in>
2023-02-16 15:11:58 +0530

    refactor publish scripts and module names

---
+++ applications/the101/android/src/main/kotlin/io/tagd/the101/android/UsageActivity.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2021 The TagD Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.tagd.the101.android
+
+import android.os.Bundle
+import io.tagd.arch.present.mvp.LifeCycleAwarePresenter
+import io.tagd.arch.present.mvp.PresentableView
+import io.tagd.android.mvp.MvpActivity
+
+interface UsageView : PresentableView {
+
+    fun showCallerView()
+}
+
+class UsagePresenter(view : UsageView) : LifeCycleAwarePresenter<UsageView>(view) {
+
+    override fun onBackPressed() {
+        view?.showCallerView()
+    }
+}
+
+class UsageActivity : MvpActivity<UsageView, UsagePresenter>(), UsageView {
+
+    override fun onCreatePresenter(savedInstanceState: Bundle?): UsagePresenter {
+        return UsagePresenter(this)
+    }
+
+    override fun onCreateView(savedInstanceState: Bundle?) {
+    }
+
+    override fun onAwaiting() {
+        super.onAwaiting()
+    }
+
+    override fun onReady() {
+        super.onReady()
+        setContentView(R.layout.usage_view)
+
+        val injector = AppInjector()
+        injector.setup(this)
+        val usage = Usage()
+        usage.use()
+        usage.release()
+    }
+
+    override fun showCallerView() {
+        finish()
+    }
+
+    override fun release() {
+    }
+}
---
+++ architecture/kmm-tagd-android/src/main/java/io/tagd/android/lifecycle/ReadyLifeCycleEventDispatcher.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2021 The TagD Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package io.tagd.android.lifecycle
+
+import io.tagd.android.launch.AppService
+import java.lang.ref.WeakReference
+import java.util.*
+
+class ReadyLifeCycleEventDispatcher : AppService {
+
+    private var queue : Queue<WeakReference<ReadyLifeCycleEventOwner>>? = ArrayDeque()
+    private var ready: Boolean = false
+
+    fun register(owner: ReadyLifeCycleEventOwner) {
+        queue?.offer(WeakReference(owner))
+    }
+
+    fun unregister(owner: ReadyLifeCycleEventOwner) {
+        queue?.firstOrNull {
+            it?.get() === owner
+        }?.let {
+            queue?.remove(it)
+        }
+    }
+
+    fun dispatchOnReady() {
+        ready = true
+        queue?.forEach {
+            it?.get()?.onReady()
+        }
+        clearRegistry()
+    }
+
+    fun ready() : Boolean {
+        return ready
+    }
+
+    private fun clearRegistry() {
+        queue?.forEach {
+            it?.clear()
+        }
+        queue?.clear()
+    }
+
+    override fun release() {
+        clearRegistry()
+        queue = null
+    }
+}
