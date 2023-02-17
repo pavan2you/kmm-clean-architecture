d6b3e212c2cc62e72cfebcdd51428066163ec9f2
pavan kunchapu <pavan.k@verse.in>
2023-02-17 19:55:54 +0530

    handle fetching repos from github packages - 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-
-    id("convention.secrets")
-}
-
-repositories {
-    google()
-    mavenCentral()
-    mavenLocal()
-    maven {
-        url = uri("https://maven.pkg.github.com/pavan2you/kmm-clean-architecture")
-
-        credentials {
-            username = extra["githubUser"] as? String
-            password = extra["githubToken"] as? String
-        }
-    }
 }
 
 tasks.register("clean", Delete::class) {
