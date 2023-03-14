ac6009a3e37980c460ae8e01c573aaadc3c7764d
pavan kunchapu <pavan.k@verse.in>
2023-03-14 18:46:27 +0530

    get process name

---
+++ language/lang-androidx/src/main/kotlin/io/tagd/androidx/os/ApplicationExt.kt
@@ -0,0 +1,26 @@
+package io.tagd.androidx.os
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.os.Build
+import java.lang.reflect.InvocationTargetException
+
+fun Application.processName(): String? {
+    return if (Build.VERSION.SDK_INT >= 28) Application.getProcessName() else try {
+        @SuppressLint("PrivateApi") val activityThread = Class.forName("android.app.ActivityThread")
+        val methodName = "currentProcessName"
+        val getProcessName = activityThread.getDeclaredMethod(methodName)
+        getProcessName.invoke(null) as String
+    } catch (e: ClassNotFoundException) {
+        throw RuntimeException(e)
+    } catch (e: NoSuchMethodException) {
+        throw RuntimeException(e)
+    } catch (e: IllegalAccessException) {
+        throw RuntimeException(e)
+    } catch (e: InvocationTargetException) {
+        throw RuntimeException(e)
+    }
+
+    // Using the same technique as Application.getProcessName() for older devices
+    // Using reflection since ActivityThread is an internal API
+}
