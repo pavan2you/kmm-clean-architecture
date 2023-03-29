968793e7406ba2a593b7d31663537a6e9f00033a
Kush Saini <kushsaini.700@gmail.com>
2023-03-29 21:00:20 +0530

    millis extensions updated

--- language/kmm-langx/src/commonMain/kotlin/io/tagd/langx/Millis.kt
+++ language/kmm-langx/src/commonMain/kotlin/io/tagd/langx/Millis.kt
@@ -4,10 +4,20 @@ data class Millis(var millis: Long = 0L)
 
 @Suppress("warnings")
 fun Millis?.isNull(): Boolean {
-    return this == null || this.millis == null
+    return this == null || this.millis.isNull()
 }
 
 @Suppress("warnings")
 fun Millis?.isNullOrZero(): Boolean {
-    return this.isNull() || this?.millis == 0L
+    return this.isNull() || this!!.millis!!.isNullOrZero()
+}
+
+@Suppress("warnings")
+fun Millis?.isNullOrLessThanZero(): Boolean {
+    return this.isNull() || this!!.millis!!.isNullOrLessThanZero()
+}
+
+@Suppress("warnings")
+fun Millis?.isNullOrLessThanEqualZero(): Boolean {
+    return this.isNull() || this!!.millis!!.isNullOrLessThanEqualZero()
 }
