ecd4186a706439904d1a8ab6342599f9db03dc6e
pavan <pavan2you@gmail.com>
2023-04-21 18:21:50 +0530

    introduce ConditionalInitializer

--- architecture/kmm-tagd-core/src/commonMain/kotlin/io/tagd/core/Initializer.kt
+++ architecture/kmm-tagd-core/src/commonMain/kotlin/io/tagd/core/Initializer.kt
@@ -1,11 +1,20 @@
 package io.tagd.core
 
-interface Initializer<T> : Releasable {
+interface Initializer<T> : Service {
 
     fun new(vararg args: Any? = arrayOf()): T
 
     interface Builder<T, I : Initializer<T>> {
 
         fun build() : I
+    }
+}
+
+interface ConditionalInitializer<T> : Initializer<T> {
+
+    fun canInitialize(): Boolean
+
+    fun conditionalNew(vararg args: Any? = arrayOf()): T? {
+        return if (canInitialize()) new(args) else null
     }
 }
