a07a87573e3c29e27146e0071ea32c7ee17f37bb
pavan <pavan2you@gmail.com>
2023-05-10 14:52:06 +0530

    write an Application extension to determin is process is main

--- language/lang-androidx/src/main/kotlin/io/tagd/androidx/app/ApplicationExtension.kt
+++ language/lang-androidx/src/main/kotlin/io/tagd/androidx/app/ApplicationExtension.kt
@@ -1,6 +1,7 @@
 package io.tagd.androidx.app
 
 import android.annotation.SuppressLint
+import android.app.ActivityManager
 import android.app.Application
 import android.os.Build
 import java.lang.reflect.InvocationTargetException
@@ -24,4 +25,18 @@ fun Application.processName(): String? {
 
     // Using the same technique as Application.getProcessName() for older devices
     // Using reflection since ActivityThread is an internal API
+}
+
+//Judge if it's in the main process
+fun Application.isMainProcess(): Boolean {
+    val am = getSystemService(android.content.Context.ACTIVITY_SERVICE) as ActivityManager?
+    val processInfoList = am?.runningAppProcesses
+    val mainProcessName: String = applicationInfo?.processName.orEmpty()
+    val myPid: Int = android.os.Process.myPid()
+    for (info in processInfoList.orEmpty()) {
+        if (info.pid == myPid && mainProcessName == info.processName) {
+            return true
+        }
+    }
+    return false
 }
