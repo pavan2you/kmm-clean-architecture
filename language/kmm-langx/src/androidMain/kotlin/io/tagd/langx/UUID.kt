1779e4d7c80a43d91d435f23c69361cb364c09b7
amit.sarkar <amit.sarkar@verse.in>
2023-06-20 12:06:41 +0530

    Added UUID fromString

--- language/kmm-langx/src/androidMain/kotlin/io/tagd/langx/UUID.kt
+++ language/kmm-langx/src/androidMain/kotlin/io/tagd/langx/UUID.kt
@@ -2,8 +2,23 @@ package io.tagd.langx
 
 actual class UUID {
 
-    private val _value = java.util.UUID.randomUUID().toString()
+    private val _value: String
 
     actual val value: String
         get() = _value
+
+    actual constructor() {
+        _value = java.util.UUID.randomUUID().toString()
+    }
+
+    actual constructor(value: String) {
+        _value = value
+    }
+
+    actual companion object {
+        actual fun fromString(name: String): UUID {
+            val uuid = java.util.UUID.fromString(name).toString()
+            return UUID(uuid)
+        }
+    }
 }
