fa7cc00970b452e9cd66c5389dc97cbca5acca9d
pavan <pavan2you@gmail.com>
2023-10-17 16:30:12 +0530

    include Bundle expect-actual support

---
+++ language/kmm-langx/src/commonMain/kotlin/io/tagd/langx/Bundle.kt
@@ -0,0 +1,20 @@
+package io.tagd.langx
+
+expect class Bundle {
+
+    fun clear()
+
+    fun remove(key: String)
+
+    fun putAll(bundle: Bundle)
+
+    fun putByte(key: String?, value: Byte)
+
+    fun putChar(key: String?, value: Char)
+
+    fun putShort(key: String?, value: Short)
+
+    fun putFloat(key: String?, value: Float)
+
+    fun putCharSequence(key: String?, value: CharSequence?)
+}
---
+++ language/kmm-langx/src/iosMain/kotlin/io/tagd/langx/Bundle.ios.kt
@@ -0,0 +1,29 @@
+package io.tagd.langx
+
+actual class Bundle {
+
+    actual fun clear() {
+    }
+
+    actual fun remove(key: String) {
+    }
+
+    actual fun putAll(bundle: Bundle) {
+    }
+
+    actual fun putByte(key: String?, value: Byte) {
+    }
+
+    actual fun putChar(key: String?, value: Char) {
+    }
+
+    actual fun putShort(key: String?, value: Short) {
+    }
+
+    actual fun putFloat(key: String?, value: Float) {
+    }
+
+    actual fun putCharSequence(key: String?, value: CharSequence?) {
+    }
+
+}
