804d98e76871176b3346880f4f4293792920aa05
pavan <pavan2you@gmail.com>
2023-11-09 13:25:25 +0530

    read raw json as jsonobject and/as jsonarray

--- architecture/kmm-tagd-android/src/main/java/io/tagd/android/resource/ResourceReader.kt
+++ architecture/kmm-tagd-android/src/main/java/io/tagd/android/resource/ResourceReader.kt
@@ -2,13 +2,19 @@ package io.tagd.android.resource
 
 import android.annotation.SuppressLint
 import android.content.Context
+import io.tagd.arch.access.crosscutting
+import io.tagd.arch.domain.crosscutting.codec.JsonCodec
 import io.tagd.arch.infra.CompressedResource
 import io.tagd.arch.infra.ICompressedResource
 import io.tagd.arch.infra.INamedResource
 import io.tagd.arch.infra.NamedResourceReader
 import io.tagd.arch.infra.UnifiedResource
 import io.tagd.arch.infra.UnifiedResourceReader
+import io.tagd.arch.infra.readCompressedJson
 import io.tagd.arch.infra.toFileNameParts
+import io.tagd.di.typeOf
+import io.tagd.langx.json.JSONArray
+import io.tagd.langx.json.JSONObject
 import java.io.File
 import java.io.FileReader
 import java.io.IOException
@@ -86,4 +92,18 @@ open class ContextResourceReader(context: Any?) : UnifiedResourceReader {
             return null
         }
     }
+}
+
+fun readCompressedJson(context: io.tagd.langx.Context, id: Int): JSONObject? {
+    return ContextResourceReader(context).readCompressed(CompressedResource(identifier = id))
+        ?.let { json ->
+            JSONObject(json)
+        }
+}
+
+fun readCompressedJsonArray(context: io.tagd.langx.Context, id: Int): JSONArray? {
+    return ContextResourceReader(context).readCompressed(CompressedResource(identifier = id))
+        ?.let { json ->
+            JSONArray(json)
+        }
 }
