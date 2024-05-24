705adf66d3447298eaa28472b5a506b061b33764
pavan <pavan2you@gmail.com>
2024-05-24 17:57:50 +0530

    include documentation for Scopable management spec

--- architecture/kmm-tagd-arch/src/commonMain/kotlin/io/tagd/arch/scopable/ScopableManagementSpec.kt
+++ architecture/kmm-tagd-arch/src/commonMain/kotlin/io/tagd/arch/scopable/ScopableManagementSpec.kt
@@ -9,12 +9,39 @@ interface ScopableManagementSpec : Service, AsyncContext {
 
     val outer: Scopable?
 
+    /**
+     * Note : The derived classes must call the super implementations at the end of the method,
+     * this is very important to invoke the [callback] at right state of the application.
+     *
+     * Alternatively,
+     *
+     * the derived classes must do the all the work of super implementations and avoid calling
+     * super method implementation.
+     */
     fun initialize(outer: Scopable, callback: Callback<Unit>)
 
+    /**
+     * Note : The derived classes must call the super implementations at the end of the method,
+     * this is very important to invoke the [callback] at right state of the application.
+     *
+     * Alternatively,
+     *
+     * the derived classes must do the all the work of super implementations and avoid calling
+     * super method implementation.
+     */
     fun <Outer : Scopable> registerLoadingSteps(
         handler: LoadingStateHandler<Outer, *, *>,
         callback: Callback<Unit>
     )
 
+    /**
+     * Note : The derived classes must call the super implementations at the end of the method,
+     * this is very important to invoke the [callback] at right state of the application.
+     *
+     * Alternatively,
+     *
+     * the derived classes must do the all the work of super implementations and avoid calling
+     * super method implementation.
+     */
     fun inject(callback: Callback<Unit>)
 }
